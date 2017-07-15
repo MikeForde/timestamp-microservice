c5a3c99475e27bbc8bcce3da6d793a58af4808ac
System <none>
2017-07-15 17:18:09 +0000

    🛣🚋 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -5,22 +5,12 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
+  console.log('client-side javascript running when page opens');
   
-  $.get('/dreams', function(dreams) {
+  /*$.get('/dreams', function(dreams) {
     dreams.forEach(function(dream) {
       $('<li></li>').text(dream).appendTo('ul#dreams');
     });
-  });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
+  });*/
 
 });
